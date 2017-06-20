8fed375cadab0fdb58d028b80f8c2bc9d5625b4c
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-06-20 18:16:18 -0500

    created models, database with categories, and items in categories

--- closetclient/urls.py
+++ closetclient/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /closetclient/
     url(r'^$', views.index, name='index'),
+    # ex: /closetclient/5/
+    url(r'^(?P<category_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /closetclient/5/results/
+    url(r'^(?P<category_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /closetclient/5/vote/
+    url(r'^(?P<category_id>[0-9]+)/vote/$', views.item_count, name='vote'),
 ]
--- closetclient/views.py
+++ closetclient/views.py
@@ -7,3 +7,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the closetclient index.")
+
+def detail(request, category_id):
+    return HttpResponse("You're looking at category %s." % category_id)
+
+def results(request, category_id):
+    response = "You're looking at the results of category %s."
+    return HttpResponse(response % category_id)
+
+def item_count(request, category_id):
+    return HttpResponse("You're item_count on category %s." % category_id)
+
