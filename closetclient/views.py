6ef331d70339c38c5c8b2554c3256824f7c630a3
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-06-21 20:41:11 -0500

    improved views, added more tests

--- closetclient/views.py
+++ closetclient/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 from .models import Category, Item
@@ -9,24 +10,39 @@ from .models import Category, Item
 
 
 class IndexView(generic.ListView):
-    ''' displays latest nine Categorys in closetclient/index.html '''
+    ''' 
+    displays latest nine Categorys in closetclient/index.html '''
     template_name = 'closetclient/index.html'
     context_object_name = 'latest_category_list'
 
     def get_queryset(self):
-        """Return the last five published Categorys."""
-        return Category.objects.order_by('-pub_date')[:9]
+        """ 
+        return the last nine published Categorys (not including those set to be
+        published in the future)
+        """
+        return Category.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:9]
 
 
 class DetailView(generic.DetailView):
-    ''' displays single Category form in closetclient/details.html '''
+    ''' 
+    displays single Category form in closetclient/details.html '''
     model = Category
     template_name = 'closetclient/detail.html'
+
+    def get_queryset(self):
+        """ 
+        excludes any Categorys that are not published yet.
+        """
+        return Category.objects.filter(pub_date__lte=timezone.now())
+
 
 
 class ResultsView(generic.DetailView):
     model = Category
     template_name = 'closetclient/results.html'
+
+
+
 
 
 # item_count action
